20610629deadc220e365e6d3337fc2fae8a5b5e7
Tim Neubauer <timnbr99@gmail.com>
2023-06-28 22:21:20 +0200

    ux fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,17 +13,14 @@
   }
   ```
 */
-	$: raceLengthMinutes = 20
 
-	$: raceLengthLaps = 10
+	let raceLengthMinutes = 20
+	let raceLengthLaps = 10
+	let fuelPerLap = 3.7
 
-	$: fuelPerLap = 3.7
-
-	$: minutes = 1
-	$: seconds = 42
-	$: milliseconds = 690
-
-	$: computedMinutes = minutes + seconds / 60 + milliseconds / 60000
+	let minutes = 1
+	let seconds = 42
+	let milliseconds = 690
 
 	$: if (minutes < 0) minutes = 0
 
@@ -33,6 +30,7 @@
 	$: if (milliseconds < 0) milliseconds = 0
 	$: if (milliseconds > 999) milliseconds = 999
 
+	$: computedMinutes = minutes + seconds / 60 + milliseconds / 60000
 	$: laps =
 		currentTab?.name === 'Time' ? Math.ceil(raceLengthMinutes / computedMinutes) : raceLengthLaps
 
@@ -59,33 +57,31 @@
 
 <main>
 	<section class="flex h-screen w-screen items-center justify-center py-24 px-4">
-		<div class="mx-auto max-w-[32rem]">
-			<div>
-				<!-- style="font-family: 'Racing Sans One'" -->
-				<p class="text-center text-lg font-light">Sim Racing</p>
-				<h1 class="mb-6 text-center text-4xl font-bold text-gray-900">Fuel Calculator</h1>
-				<nav class="isolate flex divide-x divide-gray-200 rounded-lg shadow" aria-label="Tabs">
-					{#each tabs as tab, tabIdx}
-						<button
-							on:click={() => handleTabChange(tabIdx)}
+		<div class="max-w-[32rem]">
+			<p class="text-center text-lg font-light">Sim Racing</p>
+			<h1 class="mb-6 text-center text-4xl font-bold text-gray-900">Fuel Calculator</h1>
+			<nav class="isolate flex divide-x divide-gray-200 rounded-lg shadow" aria-label="Tabs">
+				{#each tabs as tab, tabIdx}
+					<button
+						on:click={() => handleTabChange(tabIdx)}
+						class={classNames(
+							tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+							tabIdx === 0 ? 'rounded-tl-lg' : '',
+							tabIdx === tabs.length - 1 ? 'rounded-tr-lg' : '',
+							'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10'
+						)}
+						aria-current={tab.current ? 'page' : undefined}>
+						<span>{tab.name}</span>
+						<span
+							aria-hidden="true"
 							class={classNames(
-								tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
-								tabIdx === 0 ? 'rounded-tl-lg' : '',
-								tabIdx === tabs.length - 1 ? 'rounded-tr-lg' : '',
-								'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10'
-							)}
-							aria-current={tab.current ? 'page' : undefined}>
-							<span>{tab.name}</span>
-							<span
-								aria-hidden="true"
-								class={classNames(
-									tab.current ? 'bg-indigo-500' : 'bg-transparent',
-									'absolute inset-x-0 bottom-0 h-0.5'
-								)} />
-						</button>
-					{/each}
-				</nav>
-			</div>
+								tab.current ? 'bg-indigo-500' : 'bg-transparent',
+								'absolute inset-x-0 bottom-0 h-0.5'
+							)} />
+					</button>
+				{/each}
+			</nav>
+			<!-- Calculator -->
 			<div class="flex flex-col space-y-6 bg-gray-50 p-8">
 				{#if currentTab?.name === 'Time'}
 					<div class="w-full">
