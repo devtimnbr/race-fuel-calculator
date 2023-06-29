2d7dc3e3f017e602e43d89034cc4cf8705b8545c
Tim Neubauer <timnbr99@gmail.com>
2023-06-29 13:41:34 +0200

    add metatags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
 	import '../app.css'
 </script>
 
+<head>
+	<meta name="title" content="Racing Fuel Calculator | Sim Racing Fuel Management Tool" />
+	<meta
+		name="description"
+		content="Optimize your sim racing performance with our Racing Fuel Calculator. Calculate fuel requirements for games like Assetto Corsa Competizione." />
+	<meta name="robots" content="index, follow" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="English" />
+</head>
+
 <slot />
